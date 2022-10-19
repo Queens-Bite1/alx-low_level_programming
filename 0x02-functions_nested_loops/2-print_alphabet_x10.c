2449f7fc841e48c8427e352ab12c57d57c5ea112
Queens-Bite1 <iamayaaba@gmail.com>
2022-10-19 15:51:10 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10x the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char ch;
+
+	for (ten = 0; ten <= 9; ten++)
+		_putchar(ch);
+	_putchar('\n');
+}
