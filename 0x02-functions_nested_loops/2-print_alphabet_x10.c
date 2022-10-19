cac7bf4f2282dec623a9f1a44f1b0dbc4d97b879
Queens-Bite1 <iamayaaba@gmail.com>
2022-10-19 15:59:29 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,10 @@ void print_alphabet_x10(void)
 	int ten;
 	char ch;
 
-	for (ten = 0; ten <= 9; ten++)
-		_putchar(ch);
-	_putchar('\n');
+	for (ten 0; ten <= 9; ten++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_puchar('\n');
+	}
 }
